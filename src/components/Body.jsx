d77d2b101f0ac20e9f26c99d89062f9321ee14a0
Gokul Krishna <gokulkrishna434@gmail.com>
2025-01-23 22:21:53 +0530

    useState implemented, restaurant filtering added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import resList from '../utils/dummy-data';
 import RestaurantCard from './RestaurantCard';
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRes = listOfRestaurants.filter(
+              (res) => res?.info.avgRating > 4.2,
+            );
+            setListOfRestaurants(filteredRes);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
         ))}
       </div>
