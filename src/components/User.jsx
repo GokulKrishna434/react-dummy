0fac4e06ec5bdfda7363bef45900e0f09929cb0c
Gokul Krishna <gokulkrishna434@gmail.com>
2025-01-28 21:20:59 +0530

    State variables init - class

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const User = ({ name, location }) => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(1);
+
   return (
     <div className="user-card">
+      <h1>Count: {count}</h1>
+      <h1>Count2: {count2}</h1>
       <h2>Name: {name}</h2>
       <h3>Location: {location}</h3>
       <h4>Contact: @gokulkrishna</h4>
