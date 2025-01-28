9408a2b0d727c7d9aac9c519163d46fb6761e913
Gokul Krishna <gokulkrishna434@gmail.com>
2025-01-28 21:11:25 +0530

    About component - Class and function - with props

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import User from './User';
+import UserClass from './UserClass';
 
 const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <User name={'Gokul (Function)'} location={'Trivandrum Function'} />
+
+      <UserClass name={'Gokul (Class)'} location={'Trivandrum Class'} />
     </div>
   );
 };
